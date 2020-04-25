bed0b3cbb61afd3c51efca82b4a5a3af2ec0c82f
Eric Zhao <21zhaoe@protonmail.com>
2020-04-25 10:24:05 -0400

    Bump setup.py version to 0.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="beets-playlistc",
-    version="0.1.1",
+    version="0.1.3",
     description="A beets plugin to create playlists from query strings",
     license="MIT",
     long_description=long_description,
